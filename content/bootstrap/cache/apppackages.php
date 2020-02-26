ca0d99088f847fc07a6c9773d1dc44cadd0428a9
royalwang <hxroyal@gmail.com>
2020-02-27 01:02:02 +0800

    生成应用缓存

--- content/bootstrap/cache/apppackages.php
+++ content/bootstrap/cache/apppackages.php
@@ -1,2 +1,16 @@
 <?php return array (
+  'royalcms/app-memadmin' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'App\\Memadmin\\MemadminServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'App\\Memadmin\\' => '/content/apps/memadmin/classes/',
+    ),
+  ),
 );
