c654e9390d7e15196990272ab0aa31cec0132854
royalwang <hxroyal@gmail.com>
2020-02-27 17:52:05 +0800

    优化控制器逻辑解除本地网络限制

---
+++ content/apps/memadmin/classes/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Memadmin\Controllers;
+
+
+class LoginController extends FrontBase
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function init()
+    {
+
+    }
+
+}
